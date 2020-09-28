baf9ab70601505511676e55e25b7e7275c29e4c0
summerce-platform <68770005+summerce-platform@users.noreply.github.com>
2020-09-28 13:22:19 +0900

    Update ConfigureDialog.js

--- ConfigureDialog.js
+++ ConfigureDialog.js
@@ -163,9 +163,17 @@
       
       // 다이얼로그를 종료하며 closePayload 정보를 담아 부모 페이지에 전송
       tableau.extensions.ui.closeDialog(JSON.stringify(closePayload));
-      // let currentSettings = tableau.extensions.setting.getAll();
-      tableau.extensions.settings.set(worksheetSettingsKey, JSON.stringify(selectedWorksheet));
-      tableau.extensions.settings.set(columnsSettingsKey, JSON.stringify(columns));
+
+  
+        let currentSettings = tableau.extensions.settings.getAll();
+        tableau.extensions.settings.set(worksheetSettingsKey, JSON.stringify(selectedWorksheet));
+        tableau.extensions.settings.set(columnsSettingsKey, JSON.stringify(columns));
+        tableau.extensions.settings.saveAsync().then((newSavedSettings)=> {
+        tableau.extensions.ui.closeDialog("test");
+      })
+           
+
+
 
     }
   };
