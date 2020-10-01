0ff680f21502f15dcd057ed00ed31913d562d61b
summerce-platform <68770005+summerce-platform@users.noreply.github.com>
2020-10-01 15:12:54 +0900

    Update ConfigureDialog.js

--- ConfigureDialog.js
+++ ConfigureDialog.js
@@ -122,8 +122,8 @@
   });
   $("#submitCol").click(function(){
     var myColumn = $("#select-column").val();
-    var filterCol = columns.filter(function(column){
-      column.fieldName==myColumn;
+    var filterCol = columns.filter(function(myColumn){
+      return column.fieldName==myColumn;
     });
       
     console.log(filterCol);
